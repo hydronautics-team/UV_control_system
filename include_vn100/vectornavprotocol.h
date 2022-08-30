bfb599336ebc8d44516316ecab1b4550c2eb542f
Grisha Starykh <grishafv@gmail.com>
2022-08-30 18:01:45 +0300

    try to build with cMake

---
+++ include_vn100/vectornavprotocol.h
@@ -0,0 +1,69 @@
+#ifndef VECTORNAVPROTOCOL_H
+#define VECTORNAVPROTOCOL_H
+
+#include <QObject>
+#include <QTimer>
+#include <stdio.h>
+#include <unistd.h>
+#include "vectornav.h"
+#include <QThread>
+#include <QDebug>
+
+const char* const COM_PORT = "COM5";
+const int BAUD_RATE = 115200;
+
+//для использования kx-pult
+extern double X[2000][2];
+extern QVector<double> K;
+
+class VectorNavProtocol : public QObject
+{
+    Q_OBJECT
+public:
+    VectorNavProtocol(QObject * prnt = nullptr):QObject(prnt){
+        QThread * thread = new QThread();
+        moveToThread(thread);
+        thread->start();
+        vn100_connect(&vn100, COM_PORT, BAUD_RATE);
+        sleep(1000);
+        connect(&timer, &QTimer::timeout, this, &VectorNavProtocol::tick);
+    }
+    virtual ~VectorNavProtocol(){
+        vn100_disconnect(&vn100);
+    }
+public slots:
+    void start (int dt){
+        timer.start(dt);
+    }
+    void stop(){
+        timer.stop();
+    }
+    void tick(){
+       //Vn100CompositeData data;
+       //vn100_getCurrentAsyncData(&vn100, &data);
+       vn100_getYawPitchRollTrueInertialAcclerationAngularRate(&vn100,&ypr,&inertialAcceleration,&angularRate);
+       //vn100_getCalibratedImuMeasurements(&vn100,&magnetic,&inertialAcceleration,&angularRate, &temp);
+
+       //kx-pult
+        X[301][0] = ypr.yaw;
+        X[302][0] = ypr.pitch;
+        X[303][0] = ypr.roll;
+        X[304][0] = inertialAcceleration.c0;
+        X[305][0] = inertialAcceleration.c1;
+        X[306][0] = inertialAcceleration.c2;
+        X[307][0] = angularRate.c0;
+        X[308][0] = angularRate.c1;
+        X[309][0] = angularRate.c2;
+        X[310][0] = K[1]; //теперь значение этой переменной будет таким же как принятый коэффициент K1
+    }
+protected:
+    QTimer timer;
+    Vn100 vn100;
+    VnYpr ypr;
+    VnVector3 inertialAcceleration;
+    VnVector3 angularRate;
+    VnVector3 magnetic;
+    double temp;
+};
+
+#endif // VECTORNAVPROTOCOL_H
---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <QCoreApplication>
+#include "include_vn100/vectornavprotocol.h"
+#include <QThread>
+#include "kx_pult/kx_protocol.h"
+#include "kx_pult/qkx_coeffs.h"
+#include "pult_connection/pultcontrolsystemprotocols.h"
+
+const QString ConfigFile = "protocols.conf";
+const QString XI = "xi";
+const QString KI = "ki";
+
+double X[2000][2];
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    //передача K
+    Qkx_coeffs * kProtocol = new Qkx_coeffs(ConfigFile, KI);
+    //передача X
+    x_protocol * xProtocol = new x_protocol(ConfigFile,XI,X);
+    //обмен с VectorNav (там внутри пример с примененем X-ов)
+    VectorNavProtocol vn100Proto;
+    vn100Proto.start(100);
+    //обмен с пультом
+    ControlSystem::PC_Protocol *pultProtocol = new ControlSystem::PC_Protocol(ConfigFile,"rov_pult");
+    pultProtocol->send_data.imuData.ax = 1;
+    pultProtocol->send_data.imuData.ay = 2;
+    pultProtocol->send_data.imuData.az = 3;
+    pultProtocol->send_data.imuData.gamma = 4;
+    pultProtocol->send_data.imuData.psi = 5;
+    pultProtocol->send_data.imuData.teta = 6;
+    pultProtocol->send_data.imuData.q0 = 7;
+    pultProtocol->send_data.imuData.q1 = 8;
+    pultProtocol->send_data.imuData.q2 = 9;
+    pultProtocol->send_data.imuData.q3 = 10;
+    pultProtocol->send_data.depth = 11;
+    pultProtocol->send_data.connectionFlags.controlSystem = 0;
+    pultProtocol->send_data.connectionFlags.joystick = 1;
+    pultProtocol->send_data.connectionFlags.thrusterController = 0;
+    pultProtocol->send_data.connectionFlags.vectorNav = 1;
+    return a.exec();
+}
